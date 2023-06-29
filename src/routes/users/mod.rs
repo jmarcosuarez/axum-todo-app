704b5b11a7ea981ff9e4f99fbc365a80102d1799
marcos <jmarcosuarez@gmail.com>
2023-06-29 15:19:18 -0400

    Allow users to log in

--- src/routes/users/mod.rs
+++ src/routes/users/mod.rs
@@ -1,6 +1,14 @@
+use axum::http::StatusCode;
+use sea_orm::TryIntoModel;
 use serde::{Deserialize, Serialize};
 
+use crate::{
+    database::users::{self, ActiveModel},
+    utils::app_error::AppError,
+};
+
 pub mod create_user;
+pub mod login;
 
 #[derive(Serialize, Deserialize)]
 pub struct ResponseDataUser {
@@ -18,4 +26,11 @@ pub struct ResponseUser {
 pub struct RequestCreateUser {
     username: String,
     password: String,
+}
+
+fn convert_active_to_model(user: ActiveModel) -> Result<users::Model, AppError> {
+    user.try_into_model().map_err(|error| {
+        eprintln!("Error saving token to user in db {:?}", error);
+        AppError::new(StatusCode::INTERNAL_SERVER_ERROR, "Error logging you in")
+    })
 }
--- src/utils/hash.rs
+++ src/utils/hash.rs
@@ -1,10 +1,20 @@
 use super::app_error::AppError;
 use axum::http::StatusCode;
-use bcrypt::{hash, DEFAULT_COST};
+use bcrypt::{hash, verify, DEFAULT_COST};
 
 pub fn hash_password(password: &str) -> Result<String, AppError> {
     hash(password, DEFAULT_COST).map_err(|error| {
         eprintln!("Error hashing password: {:?}", error);
         AppError::new(StatusCode::INTERNAL_SERVER_ERROR, "Error securing password")
+    })
+}
+
+pub fn verify_password(password: &str, hash: &str) -> Result<bool, AppError> {
+    verify(password, hash).map_err(|error| {
+        eprintln!("Error verifying password: {:?}", error);
+        AppError::new(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "There as a problem verifying your password",
+        )
     })
 }
