19bb75e9bb549e92555fd3b0c58858fdfe8e8178
Marcos Suarez <msrxse@gmail.com>
2023-07-27 16:39:58 +0100

    Update tasks

--- src/router.rs
+++ src/router.rs
@@ -4,14 +4,17 @@ use crate::{
     routes::{
         hello_world::hello_world,
         tasks::{
-            create_task::create_task, get_all_tasks::get_all_tasks, get_one_task::get_one_task,
+            create_task::create_task,
+            get_all_tasks::get_all_tasks,
+            get_one_task::get_one_task,
+            update_tasks::{mark_completed, mark_uncompleted, update_task},
         },
         users::{create_user::create_user, login::login, logout::logout},
     },
 };
 use axum::{
     middleware::{self},
-    routing::{get, post},
+    routing::{get, patch, post, put},
     Router,
 };
 
@@ -21,6 +24,12 @@ pub fn create_router(app_state: AppState) -> Router {
         .route("/api/v1/tasks", post(create_task))
         .route("/api/vi/tasks", get(get_all_tasks))
         .route("/api/v1/tasks/:task_id", get(get_one_task))
+        .route("/api/v1/tasks/:task_id/completed", put(mark_completed))
+        .route(
+            "/api/v1/tasks/:task_id/uncompleted",
+            patch(mark_uncompleted),
+        )
+        .route("/api/v1/tasks/:task_id", put(update_task))
         .route_layer(middleware::from_fn_with_state(
             app_state.clone(),
             require_authentication,
--- src/routes/tasks/mod.rs
+++ src/routes/tasks/mod.rs
@@ -2,14 +2,22 @@ pub mod create_task;
 pub mod create_task_extractor;
 pub mod get_all_tasks;
 pub mod get_one_task;
+pub mod update_tasks;
 
+use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+// https://docs.rs/serde_with/3.1.0/serde_with/attr.skip_serializing_none.html
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize)]
 pub struct RequestTask {
-    pub title: String,
-    pub description: Option<String>,
-    pub priority: Option<String>,
+    pub priority: Option<Option<String>>,
+    // Always serialize next field even if None
+    #[serialize_always]
+    pub title: Option<String>,
+    pub description: Option<Option<String>>,
+    pub completed_at: Option<Option<DateTime<FixedOffset>>>,
 }
 
 #[derive(Serialize, Deserialize)]
